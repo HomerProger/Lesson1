ea096cb0a21fbad7c7a63d3f4727fb8b9f84e7b8
HomerProger <mihailap@mail.ru>
2020-08-21 09:16:22 +0300

    Lesson2HW

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TextView descriptionTextView=findViewById(R.id.city_name);
+        Button buttonChooseCity=findViewById(R.id.button_choose_city);
+        buttonChooseCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChooseCity.class));
+            }
+        });
     }
 }
